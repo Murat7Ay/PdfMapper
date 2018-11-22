84d2858e7c3592e8eb0e08420b51dcefbc6f3903
Murat Ay <murataybtp@gmail.com>
2018-11-22 19:20:26 +0300

    commit

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
-document.querySelector("#pdf-upload").addEventListener("change", function(e){
-	var canvasElement = document.querySelector("canvas")
-	var file = e.target.files[0]
-	if(file.type != "application/pdf"){
-		console.error(file.name, "is not a pdf file.")
-		return
-	}
-	
-  var muratPdf;
-  var muratPdfNumbers;
-  var muratPageNumberNow;
-	var fileReader = new FileReader();  
+window.removeResizeDiv = function(e){
+  console.log(e)
+  var parentDiv = e.offsetParent; parentDiv.parentNode.removeChild(parentDiv);
+}
 
-  window.goBack= function goBack(){
+document.querySelector('#createbtn').addEventListener('click',function(e){
+  console.log(e)
+  var coloumnName = document.querySelector('#kolon')
+  document.getElementById('canvas-wrap').innerHTML += '<div class="resize-drag"><span class="close" onclick="removeResizeDiv(this)">&times;</span></div>';
+  changePageNumber(muratPageNumberNow)
+})
+
+var muratPdf;
+var muratPdfNumbers;
+var muratPageNumberNow;
+
+window.goBack= function goBack(){
     if(muratPageNumberNow === 1){
       return;
     }
@@ -35,9 +38,8 @@ document.querySelector("#pdf-upload").addEventListener("change", function(e){
       pdf.getPage(pageNumber).then(function(page) {
         // you can now use *page* here
         var viewport = page.getViewport(1.3338);
-        console.log(viewport)
-        //viewport.height=1122.519685039;
-        //viewport.width=793.700787402;
+        viewport.height=1122.519685039;
+        viewport.width=793.700787402;
         var canvas = document.querySelector("canvas")
         canvas.height = viewport.height;
         canvas.width = viewport.width;
@@ -52,6 +54,19 @@ document.querySelector("#pdf-upload").addEventListener("change", function(e){
     });
   }
 
+document.querySelector("#pdf-upload").addEventListener("change", function(e){
+	var canvasElement = document.querySelector("canvas")
+	var file = e.target.files[0]
+	if(file.type != "application/pdf"){
+		console.error(file.name, "is not a pdf file.")
+		return
+	}
+	
+ 
+	var fileReader = new FileReader();  
+
+  
+
 	fileReader.onload = function() {
 		var typedarray = new Uint8Array(this.result);
     muratPdf = typedarray;
@@ -59,15 +74,13 @@ document.querySelector("#pdf-upload").addEventListener("change", function(e){
 			// you can now use *pdf* here
 			console.log("the pdf has ",pdf.numPages, "page(s).")
       muratPageNumberNow = 1;
-      console.log(muratPageNumberNow)
       muratPdfNumbers = pdf.numPages;
 
 			pdf.getPage(1).then(function(page) {
 				// you can now use *page* here
 				var viewport = page.getViewport(1.3338);
-				console.log(viewport)
-				//viewport.height=1122.519685039;
-				//viewport.width=793.700787402;
+				viewport.height=1122.519685039;
+				viewport.width=793.700787402;
 				var canvas = document.querySelector("canvas")
 				canvas.height = viewport.height;
 				canvas.width = viewport.width;
@@ -84,21 +97,11 @@ document.querySelector("#pdf-upload").addEventListener("change", function(e){
 
 	fileReader.readAsArrayBuffer(file);
 })
-//554.5--37.795
+
+
 
  function dragMoveListener (event) {
-
-  	var div = document.getElementsByClassName('resize-drag')[0];
- 	
-
-	var parentPos = document.getElementById('canvas-wrap').getBoundingClientRect(),
-    	childrenPos = div.getBoundingClientRect(),
-    	relativePos = {};
-
-	relativePos.X =  Math.round((childrenPos.left - parentPos.left+Math.round(div.offsetWidth/2))*72/96) //595,5
-	relativePos.Y = Math.abs( Math.round((childrenPos.top - parentPos.top+Math.round(div.offsetHeight/2))*72/96)-841.5) //841,5
-	document.getElementById('xy').innerHTML = "X:"+relativePos.X +"--"+"Y:"+relativePos.Y;
-	console.log(relativePos)
+  var div = document.getElementsByClassName('resize-drag')[0];
 
     var target = event.target,
         // keep the dragged position in the data-x/data-y attributes
@@ -120,11 +123,12 @@ document.querySelector("#pdf-upload").addEventListener("change", function(e){
 
 interact('.draggable')
   .draggable({
+    ignoreFrom: '.ignore',
     // enable inertial throwing
     inertia: true,
     // keep the element within the area of it's parent
     restrict: {
-      restriction: "parent",
+      //restriction: "parent",
       endOnly: true,
       elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
     },
@@ -133,17 +137,8 @@ interact('.draggable')
 
     // call this function on every dragmove event
     onmove: dragMoveListener,
-    // call this function on every dragend event
-    onend: function (event) {
-      var textEl = event.target.querySelector('p');
-
-      textEl && (textEl.textContent =
-        'moved a distance of '
-        + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-                     Math.pow(event.pageY - event.y0, 2) | 0))
-            .toFixed(2) + 'px');
-    }
   });
+  
 
    interact('.resize-drag')
   .draggable({
@@ -171,6 +166,7 @@ interact('.draggable')
     inertia: true,
   })
   .on('resizemove', function (event) {
+    
     var target = event.target,
         x = (parseFloat(target.getAttribute('data-x')) || 0),
         y = (parseFloat(target.getAttribute('data-y')) || 0);
@@ -186,8 +182,37 @@ interact('.draggable')
 
     target.style.webkitTransform = target.style.transform =
         'translate(' + x + 'px,' + y + 'px)';
-
+    
     target.setAttribute('data-x', x);
     target.setAttribute('data-y', y);
-    target.textContent = Math.round(event.rect.width*72/96) + '\u00D7' + Math.round(event.rect.height*72/96);
-  });
+    //target.textContent =  Math.round(event.rect.width*72/96) + '\u00D7' + Math.round(event.rect.height*72/96);
+  })
+  .on('resizeend',getPointXY)
+  .on('dragend',getPointXY);
+
+
+function getPointXY(event){
+
+  var target = event.currentTarget;
+  var div = document.getElementsByClassName('resize-drag')[0];
+  var parentPos = document.getElementById('canvas-wrap').getBoundingClientRect(),
+      childrenPos = target.getBoundingClientRect(),
+      relativePos = {};
+
+  relativePos.centerX =  Math.round((childrenPos.left - parentPos.left+Math.round(div.offsetWidth/2))*72/96) //595,5
+  relativePos.centerY =Math.round( Math.abs( Math.round((childrenPos.top - parentPos.top+Math.round(div.offsetHeight/2))*72/96)-841.5)) //841,5
+  relativePos.y1 = Math.round(Math.abs( Math.round(childrenPos.top - parentPos.top)*72/96 -841.5)),
+  relativePos.x1 = Math.abs( Math.round(parentPos.right - childrenPos.right)*72/96-595.5),
+  relativePos.y0 = Math.round(Math.round(Math.round(parentPos.bottom-childrenPos.bottom)*72/96)),
+  relativePos.x0 = Math.round(Math.round(childrenPos.left - parentPos.left)*72/96);
+
+
+    target.setAttribute('data-y1', relativePos.y1);
+    target.setAttribute('data-x1', relativePos.x1);
+    target.setAttribute('data-y0', relativePos.y0);
+    target.setAttribute('data-x0', relativePos.x0);
+    target.setAttribute('data-xCenter', relativePos.centerX);
+    target.setAttribute('data-yCenter', relativePos.centerY);
+
+  return relativePos;
+}
